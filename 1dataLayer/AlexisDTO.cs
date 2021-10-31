e814831263a488028d4af38f7c2756118ce60462
Alexis ortega <ortega.alexis@uabc.edu.mx>
2021-10-30 17:35:22 -0700

    DTO
    madre_hijo, madre, enfermedades, alergias

--- 1dataLayer/AlexisDTO.cs
+++ 1dataLayer/AlexisDTO.cs
@@ -6,17 +6,14 @@ using System.Threading.Tasks;
 
 namespace _1dataLayer
 {
-    class AlexisDTO
-    {
-    }
 
     public class alumno
     {
-        int id_alumno { get; set; }
-        
-        DateTime fecha_registo { get; }
+        int id_alumno { get; }
+
+        DateTime fecha_registo { get; set; }
         string ciclo_escolar { get; set; }
-       string nombre { get; set; }
+        string nombre { get; set; }
         string apellido_mat { get; set; }
         string apellido_pat { get; set; }
 
@@ -33,8 +30,36 @@ namespace _1dataLayer
 
         string documentacion_alumno { get; set; }
 
-       string atendido_por { get; set; }
+        string atendido_por { get; set; }
     }
+    public class madre_hijo
+    {
+        int id_alumno { get; }
+        int id_madre { get; }
+    }
+    public class madre
+    {
+        int id_madre { get; }
+        string Nombre { get; set; }
+        string apellido_mat { get; set; }
+        string apellido_pat { get; set; }
+        string colonia_madre { get; set; }
+        string calle_madre { get; set; }
+        string numero_madre { get; set; }
+        string ocupacion_madre { get; set; }
+        string colonia_trabajo_madre { get; set; }
+        string calle_trabajo_madre { get; set; }
+        string numero_trabajo_madre { get; set; }
+
+    }
+    public class telefono_madre {
+        int id_madre { get; }
+
+       int   id_telefono{get;}
+        string telefono { get; set; }    
+    }
+    
+
     public class info_medica
     {
         int id_alumno { get; }
@@ -43,6 +68,16 @@ namespace _1dataLayer
             string grupo_sanguineo { get; set; }
             string telefono_contacto { get; set; }
 
+    }
+    public class enfermedades
+    {
+        int id_cartilla_medica { get; }
+        string enfermedad { get; set; }
+    }
+    public class alergias
+    {
+        int id_cartilla_medica { get; }
+        string alergia { get; set; }
     }
 
 }
