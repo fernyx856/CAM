9c267482d8c2ba03f0086cfbf194ae92f07735a1
Alexis ortega <ortega.alexis@uabc.edu.mx>
2021-10-30 17:16:53 -0700

    DTO
    DTO alumno, cartilla_medica

--- 1dataLayer/AlexisDTO.cs
+++ 1dataLayer/AlexisDTO.cs
@@ -9,4 +9,40 @@ namespace _1dataLayer
     class AlexisDTO
     {
     }
+
+    public class alumno
+    {
+        int id_alumno { get; set; }
+        
+        DateTime fecha_registo { get; }
+        string ciclo_escolar { get; set; }
+       string nombre { get; set; }
+        string apellido_mat { get; set; }
+        string apellido_pat { get; set; }
+
+        DateTime fecha_nacimiento { get; set; }
+        string edad_alumno { get; set; }
+        string estado_nacimiento_alumno { get; set; }
+        string ciudad_nacimiento_alumno { get; set; }
+        string colonia_alumno { get; set; }
+        string calle_alumno { get; set; }
+        string numero_alumno { get; set; }
+        string telefono_personal_alumno { get; set; }
+        string discapacidad { get; set; }
+        string escuela_procedencia_alumno { get; set; }
+
+        string documentacion_alumno { get; set; }
+
+       string atendido_por { get; set; }
+    }
+    public class info_medica
+    {
+        int id_alumno { get; }
+        int id_cartilla_medica { get; set; }
+            string servicio_medico { get; set; }
+            string grupo_sanguineo { get; set; }
+            string telefono_contacto { get; set; }
+
+    }
+
 }
